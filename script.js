4403372dfba09bb1ae6b8b2f5dd9c97a4212896a
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-15 16:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const output = document.getElementById("output");
 
-// Loading row WITH ID
+// Loading row
 output.innerHTML = `
   <tr id="loading">
     <td colspan="2">Loading...</td>
@@ -8,40 +8,37 @@ output.innerHTML = `
 `;
 
 function createPromise() {
-  const time = Math.random() * 2 + 1; // 1 to 3 sec
+  const time = Math.random() * 2 + 1; // 1–3 sec
   return new Promise(resolve => {
     setTimeout(() => resolve(time), time * 1000);
   });
 }
 
-const p1 = createPromise();
-const p2 = createPromise();
-const p3 = createPromise();
+// Track start time
+const start = performance.now();
 
-Promise.all([p1, p2, p3]).then(times => {
+Promise.all([createPromise(), createPromise(), createPromise()])
+  .then(times => {
 
-  const t1 = times[0].toFixed(3);
-  const t2 = times[1].toFixed(3);
-  const t3 = times[2].toFixed(3);
+    const end = performance.now();
+    const totalTime = ((end - start) / 1000).toFixed(3);
 
-  const maxTime = Math.max(times[0], times[1], times[2]).toFixed(3);
-
-  output.innerHTML = `
-    <tr>
-      <td>Promise 1</td>
-      <td>${t1}</td>
-    </tr>
-    <tr>
-      <td>Promise 2</td>
-      <td>${t2}</td>
-    </tr>
-    <tr>
-      <td>Promise 3</td>
-      <td>${t3}</td>
-    </tr>
-    <tr>
-      <td>Total</td>
-      <td>${maxTime}</td>
-    </tr>
-  `;
-});
+    output.innerHTML = `
+      <tr>
+        <td>Promise 1</td>
+        <td>${times[0].toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Promise 2</td>
+        <td>${times[1].toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Promise 3</td>
+        <td>${times[2].toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Total</td>
+        <td>${totalTime}</td>
+      </tr>
+    `;
+  });
